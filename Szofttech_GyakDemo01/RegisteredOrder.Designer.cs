5632452a57208371599633701a55ecd0b98603a0
KKrisztofer <kerekes.krisztofer@student.uni-miskolc.hu>
2022-01-09 20:47:31 +0100

    Pro vagyok ehhez

--- Szofttech_GyakDemo01/RegisteredOrder.Designer.cs
+++ Szofttech_GyakDemo01/RegisteredOrder.Designer.cs
@@ -31,12 +31,23 @@ namespace Szofttech_GyakDemo01
         {
             this.checkedListBoxControl1 = new DevExpress.XtraEditors.CheckedListBoxControl();
             this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
-            this.checkedListBoxControl2 = new DevExpress.XtraEditors.CheckedListBoxControl();
             this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
             this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
             this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.radioGroup1 = new DevExpress.XtraEditors.RadioGroup();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.listBoxControl1 = new DevExpress.XtraEditors.ListBoxControl();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            this.radioGroup2 = new DevExpress.XtraEditors.RadioGroup();
+            this.simpleButton4 = new DevExpress.XtraEditors.SimpleButton();
             ((System.ComponentModel.ISupportInitialize)(this.checkedListBoxControl1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.checkedListBoxControl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioGroup1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBoxControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radioGroup2.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // checkedListBoxControl1
@@ -45,11 +56,13 @@ namespace Szofttech_GyakDemo01
             | System.Windows.Forms.AnchorStyles.Right)));
             this.checkedListBoxControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.checkedListBoxControl1.Appearance.Options.UseFont = true;
+            this.checkedListBoxControl1.CheckOnClick = true;
             this.checkedListBoxControl1.Location = new System.Drawing.Point(617, 43);
             this.checkedListBoxControl1.MultiColumn = true;
             this.checkedListBoxControl1.Name = "checkedListBoxControl1";
             this.checkedListBoxControl1.Size = new System.Drawing.Size(407, 473);
             this.checkedListBoxControl1.TabIndex = 20;
+            this.checkedListBoxControl1.ItemCheck += new DevExpress.XtraEditors.Controls.ItemCheckEventHandler(this.checkedListBoxControl1_ItemCheck);
             // 
             // labelControl4
             // 
@@ -60,15 +73,6 @@ namespace Szofttech_GyakDemo01
             this.labelControl4.Size = new System.Drawing.Size(50, 18);
             this.labelControl4.TabIndex = 16;
             this.labelControl4.Text = "Menük";
-            // 
-            // checkedListBoxControl2
-            // 
-            this.checkedListBoxControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.checkedListBoxControl2.Appearance.Options.UseFont = true;
-            this.checkedListBoxControl2.Location = new System.Drawing.Point(106, 43);
-            this.checkedListBoxControl2.Name = "checkedListBoxControl2";
-            this.checkedListBoxControl2.Size = new System.Drawing.Size(393, 473);
-            this.checkedListBoxControl2.TabIndex = 15;
             // 
             // labelControl3
             // 
@@ -92,6 +96,7 @@ namespace Szofttech_GyakDemo01
             this.simpleButton1.Size = new System.Drawing.Size(157, 66);
             this.simpleButton1.TabIndex = 13;
             this.simpleButton1.Text = "Rendelés leadása";
+            this.simpleButton1.Visible = false;
             this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
             // 
             // labelControl2
@@ -100,28 +105,115 @@ namespace Szofttech_GyakDemo01
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.labelControl2.Appearance.Options.UseFont = true;
-            this.labelControl2.Location = new System.Drawing.Point(1124, 674);
+            this.labelControl2.Location = new System.Drawing.Point(1090, 674);
             this.labelControl2.Name = "labelControl2";
             this.labelControl2.Size = new System.Drawing.Size(15, 29);
             this.labelControl2.TabIndex = 21;
             this.labelControl2.Text = "0";
+            // 
+            // radioGroup1
+            // 
+            this.radioGroup1.Location = new System.Drawing.Point(106, 43);
+            this.radioGroup1.Name = "radioGroup1";
+            this.radioGroup1.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.radioGroup1.Properties.Appearance.Options.UseFont = true;
+            this.radioGroup1.Properties.GlyphAlignment = DevExpress.Utils.HorzAlignment.Default;
+            this.radioGroup1.Size = new System.Drawing.Size(336, 473);
+            this.radioGroup1.TabIndex = 22;
+            this.radioGroup1.SelectedIndexChanged += new System.EventHandler(this.radioGroup1_SelectedIndexChanged);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.simpleButton2.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.simpleButton2.Appearance.Options.UseFont = true;
+            this.simpleButton2.Location = new System.Drawing.Point(1197, 660);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(157, 66);
+            this.simpleButton2.TabIndex = 23;
+            this.simpleButton2.Text = "Rendelés összesítése";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // listBoxControl1
+            // 
+            this.listBoxControl1.Location = new System.Drawing.Point(12, 709);
+            this.listBoxControl1.Name = "listBoxControl1";
+            this.listBoxControl1.Size = new System.Drawing.Size(15, 17);
+            this.listBoxControl1.TabIndex = 24;
+            this.listBoxControl1.Visible = false;
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.simpleButton4);
+            this.groupControl1.Controls.Add(this.radioGroup2);
+            this.groupControl1.Controls.Add(this.simpleButton3);
+            this.groupControl1.Controls.Add(this.labelControl1);
+            this.groupControl1.Location = new System.Drawing.Point(404, 190);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(540, 411);
+            this.groupControl1.TabIndex = 25;
+            this.groupControl1.Text = "Össszesítés";
+            this.groupControl1.Visible = false;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(14, 39);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(370, 18);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "Válassza ki azt a terméket amit ki szeretne venni:";
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.Location = new System.Drawing.Point(254, 347);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(130, 49);
+            this.simpleButton3.TabIndex = 2;
+            this.simpleButton3.Text = "Tovább a leadáshoz";
+            this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click);
+            // 
+            // radioGroup2
+            // 
+            this.radioGroup2.Location = new System.Drawing.Point(14, 81);
+            this.radioGroup2.Name = "radioGroup2";
+            this.radioGroup2.Size = new System.Drawing.Size(512, 222);
+            this.radioGroup2.TabIndex = 3;
+            // 
+            // simpleButton4
+            // 
+            this.simpleButton4.Location = new System.Drawing.Point(396, 347);
+            this.simpleButton4.Name = "simpleButton4";
+            this.simpleButton4.Size = new System.Drawing.Size(130, 49);
+            this.simpleButton4.TabIndex = 4;
+            this.simpleButton4.Text = "Kivétel";
+            this.simpleButton4.Click += new System.EventHandler(this.simpleButton4_Click);
             // 
             // RegisteredOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1366, 738);
+            this.Controls.Add(this.groupControl1);
+            this.Controls.Add(this.listBoxControl1);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.radioGroup1);
             this.Controls.Add(this.labelControl2);
             this.Controls.Add(this.checkedListBoxControl1);
             this.Controls.Add(this.labelControl4);
-            this.Controls.Add(this.checkedListBoxControl2);
             this.Controls.Add(this.labelControl3);
             this.Controls.Add(this.simpleButton1);
             this.Name = "RegisteredOrder";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "RegisteredOrder";
             ((System.ComponentModel.ISupportInitialize)(this.checkedListBoxControl1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.checkedListBoxControl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioGroup1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBoxControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            this.groupControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radioGroup2.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -131,9 +223,16 @@ namespace Szofttech_GyakDemo01
 
         private DevExpress.XtraEditors.CheckedListBoxControl checkedListBoxControl1;
         private DevExpress.XtraEditors.LabelControl labelControl4;
-        private DevExpress.XtraEditors.CheckedListBoxControl checkedListBoxControl2;
         private DevExpress.XtraEditors.LabelControl labelControl3;
         private DevExpress.XtraEditors.SimpleButton simpleButton1;
         private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.RadioGroup radioGroup1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.ListBoxControl listBoxControl1;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private DevExpress.XtraEditors.RadioGroup radioGroup2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton4;
     }
 }
--- Szofttech_GyakDemo01/RegisteredOrder.cs
+++ Szofttech_GyakDemo01/RegisteredOrder.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DevExpress.XtraEditors.Controls;
 
 namespace Szofttech_GyakDemo01
 {
@@ -17,6 +18,29 @@ namespace Szofttech_GyakDemo01
         {
 
             InitializeComponent();
+            var mainDataFromXML = ReadXML.ReadRealXML.XMLRead("readxml.xml");
+            int counter = 0;
+
+            List<string> Menus = new List<string>();
+            for (int i = 0; i < mainDataFromXML.Adatok.Count; i++)
+            {
+                Menus.Add(mainDataFromXML.Adatok[i].Menuk.Elnevezes);
+            }
+            List<string> noDuplicatesMenus = Menus.Distinct().ToList();
+            while (counter < noDuplicatesMenus.Count)
+            {
+                radioGroup1.Properties.Items.Add(new RadioGroupItem(noDuplicatesMenus[counter], noDuplicatesMenus[counter].ToString()));
+                counter++;
+            }
+            radioGroup1.SelectedIndex = 0;
+            radioGroup1.Enabled = true;
+            checkedListBoxControl1.Enabled = false;
+            counter = 0;
+            while (counter < mainDataFromXML.Adatok.Count)
+            {
+                checkedListBoxControl1.Items.Add(mainDataFromXML.Adatok[counter].Termekek.Elnevezes);                
+                counter++;
+            }
         }
 
         private void simpleButton1_Click(object sender, EventArgs e)
@@ -26,7 +50,7 @@ namespace Szofttech_GyakDemo01
             {
                 try
                 {
-                    //Rendelésleadás metódus/class meghívás
+
                 }
                 catch (Exception)
                 {
@@ -35,6 +59,110 @@ namespace Szofttech_GyakDemo01
                 }
                 MessageBox.Show("A rendelést sikeresen leadta!", "Rendelés", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+        }
+
+        private void checkedListBoxControl1_ItemCheck(object sender, DevExpress.XtraEditors.Controls.ItemCheckEventArgs e)
+        {
+            var mainDataFromXML = ReadXML.ReadRealXML.XMLRead("readxml.xml");
+            if (simpleButton1.Visible == false)
+            { 
+                double endosszeg = Convert.ToDouble(labelControl2.Text);
+                if (checkedListBoxControl1.Items[checkedListBoxControl1.SelectedIndex].CheckState == CheckState.Checked)
+                {
+                    if (radioGroup1.SelectedIndex == 0)
+                    {
+                        endosszeg += Convert.ToInt32(mainDataFromXML.Adatok[checkedListBoxControl1.SelectedIndex].Termekek.Ar.Split(' ')[0]) * 0.7;
+                        listBoxControl1.Items.Add(mainDataFromXML.Adatok[checkedListBoxControl1.SelectedIndex].Termekek.Elnevezes + " " + radioGroup1.Properties.Items[radioGroup1.SelectedIndex] + " " + "True");
+                    }
+                    else
+                    {
+                        endosszeg += Convert.ToInt32(mainDataFromXML.Adatok[checkedListBoxControl1.SelectedIndex].Termekek.Ar.Split(' ')[0]);
+                        listBoxControl1.Items.Add(mainDataFromXML.Adatok[checkedListBoxControl1.SelectedIndex].Termekek.Elnevezes + " " + radioGroup1.Properties.Items[radioGroup1.SelectedIndex] + " " + "False");
+                    }
+                }
+                else
+                {
+                    if (radioGroup1.SelectedIndex == 0)
+                    {
+                        endosszeg -= Convert.ToInt32(mainDataFromXML.Adatok[checkedListBoxControl1.SelectedIndex].Termekek.Ar.Split(' ')[0]) * 0.7;
+                    }
+                    else
+                    {
+                        endosszeg -= Convert.ToInt32(mainDataFromXML.Adatok[checkedListBoxControl1.SelectedIndex].Termekek.Ar.Split(' ')[0]);
+                    }
+                }
+
+                labelControl2.Text = (endosszeg).ToString();
+                radioGroup1.Enabled = true;
+                checkedListBoxControl1.Enabled = false;
+            }
+        }
+
+        private void radioGroup1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            radioGroup1.Enabled = false;
+            checkedListBoxControl1.Enabled = true;
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Biztosan végzett a termékek felvételével?", "Összesítés", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                simpleButton1.Visible = true;
+                simpleButton2.Visible = false;
+                checkedListBoxControl1.Enabled = false;
+                int counter = 0;
+                while (counter < checkedListBoxControl1.CheckedItems.Count)
+                {
+                    radioGroup2.Properties.Items.Add(new RadioGroupItem(checkedListBoxControl1.CheckedItems[counter], checkedListBoxControl1.CheckedItems[counter].ToString()));
+                    counter++;
+                }
+            }
+            groupControl1.Visible = true;
+            radioGroup1.Enabled = false;
+            checkedListBoxControl1.Enabled = true;
+
+        }
+        private void simpleButton4_Click(object sender, EventArgs e)
+        {
+            var mainDataFromXML = ReadXML.ReadRealXML.XMLRead("readxml.xml");
+            int counter = 0;
+            if (radioGroup2.Properties.Items.Count != 0)
+            {
+                if (simpleButton1.Visible == true)
+                {
+                    double endosszeg = Convert.ToDouble(labelControl2.Text);
+                    while (counter < mainDataFromXML.Adatok.Count)
+                    {
+                        if (listBoxControl1.Items[radioGroup2.SelectedIndex].ToString().Split(' ')[0] == mainDataFromXML.Adatok[counter].Termekek.Elnevezes)
+                        {
+                            if (listBoxControl1.Items[radioGroup2.SelectedIndex].ToString().Split(' ')[2] == "True")
+                            {
+                                endosszeg -= Convert.ToDouble(mainDataFromXML.Adatok[counter].Termekek.Ar.ToString().Split(' ')[0]) * 0.7;
+                                break;
+                            }
+                            else
+                            {
+                                endosszeg -= Convert.ToDouble(mainDataFromXML.Adatok[counter].Termekek.Ar.ToString().Split(' ')[0]);
+                                break;
+                            }
+                        }
+                        counter++;
+                    }
+                    radioGroup2.Properties.Items.Remove(radioGroup2.Properties.Items[radioGroup2.SelectedIndex]);
+                    labelControl2.Text = endosszeg.ToString();
+                }
+                else
+                {
+                    simpleButton4.Enabled = false;
+                }
+            }
+        }
+
+        private void simpleButton3_Click(object sender, EventArgs e)
+        {
+            groupControl1.Visible = false;
         }
     }
 }
